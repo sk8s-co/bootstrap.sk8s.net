ef07d8e61b354095f08998fa4ce532faef7a2ea0
Christian Nuss <christian.nuss@gmail.com>
2026-02-04 05:54:37 -0500

    fix linting

--- src/_sh/kubelet.sh
+++ src/_sh/kubelet.sh
@@ -4,7 +4,7 @@ set -eu
 # Pretty print the execution
 echo "kubelet.sh (bootstrap.sk8s.net) >>>" >&2
 echo "  Kubelet Port: ${KUBELET_PORT}" >&2
-echo "  Kubelet Arguments: $@" >&2
+echo "  Kubelet Arguments: $*" >&2
 echo "" >&2
 
 exec concurrently \
