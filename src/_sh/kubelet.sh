bdd1365f31c75090341c495bf744755167aef60c
Christian Nuss <christian.nuss@gmail.com>
2026-02-06 08:43:39 -0500

    Revert "download stop script to /usr/local/bin for teardown"
    This reverts commit 7889e900082aef91ab921b16ba336af3686ed69f.

--- src/_sh/kubelet.sh
+++ src/_sh/kubelet.sh
@@ -4,12 +4,9 @@ set -eu
 RUN +env https://bootstrap.sk8s.net/.assert.sh
 eval "$(RUN +env https://bootstrap.sk8s.net/.env.sh)"
 
-RUN +env +raw https://bootstrap.sk8s.net/.stop.sh > /usr/local/bin/stop
-chmod +x /usr/local/bin/stop
-
 exec concurrently -P \
 --names "tunnel,cri,kubelet" \
---teardown "stop" \
+--teardown "RUN +env https://bootstrap.sk8s.net/.stop.sh" \
 "RUN +env https://bootstrap.sk8s.net/.tunnel.sh ${PROVIDER_TUNNEL:-cloudflare} ${KUBELET_PORT}" \
 "RUN +env https://bootstrap.sk8s.net/.cri.sh ${PROVIDER_CRI:-docker}" \
 "RUN +env https://bootstrap.sk8s.net/.kubelet.sh {*}" \
