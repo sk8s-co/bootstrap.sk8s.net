714762d7091ea7f3f73138ac289120a9a35d5fad
Christian Nuss <christian.nuss@gmail.com>
2026-02-07 17:16:31 -0500

    predownload stop

--- src/_sh/kubelet.sh
+++ src/_sh/kubelet.sh
@@ -4,9 +4,12 @@ set -eu
 RUN +env https://bootstrap.sk8s.net/.assert.sh
 eval "$(RUN +env https://bootstrap.sk8s.net/.env.sh)"
 
+RUN +env +raw https://bootstrap.sk8s.net/.stop.sh > /bin/stop
+chmod +x /bin/stop
+
 exec concurrently -P \
 --names "tunnel,cri,kubelet" \
---teardown "RUN +env https://bootstrap.sk8s.net/.stop.sh" \
+--teardown "stop" \
 "RUN +env https://bootstrap.sk8s.net/.tunnel.sh ${PROVIDER_TUNNEL:-cloudflare} ${KUBELET_PORT}" \
 "RUN +env https://bootstrap.sk8s.net/.cri.sh ${PROVIDER_CRI:-docker}" \
 "RUN +env https://bootstrap.sk8s.net/.kubelet.sh {*}" \
